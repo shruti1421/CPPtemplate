3660d7fce744f98a7cc617a009dda6a5c80b620a
Smriti Naik <64346825+shruti1421@users.noreply.github.com>
2020-12-19 19:39:35 +0530

    Update 1st_Template.cpp

--- 1st_Template.cpp
+++ 1st_Template.cpp
@@ -8,7 +8,6 @@ using namespace std;
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
 #define all(x) x.begin(), x.end()
 #define clr(x) memset(x, 0, sizeof(x))
-#define sortall(x) sort(all(x))
 #define PI 3.1415926535897932384626
 typedef pair<int, int>	pii;
 typedef vector<int>		vi;
@@ -91,12 +90,13 @@ ll n, m, k;
 
 void solve() {
   cin >> n >> m;
-  cout << countDigits(n) <<'\n';
-  cout << gcd(n,m) << '\n';
-  cout << lcm(n,m) << '\n';
-  cout << isPrime(n) << isPrime(m) << '\n';
-  cout << power(n,m) << '\n';
-  cout << setBits(n) << '\n';
+  
+    
+  int i=0;
+  fo(i,n)
+  {
+     
+  }
   
 }
 
@@ -104,7 +104,7 @@ int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
     int t = 1;
-    //cin >> t;
+    cin >> t;
     while(t--) {
       solve();
     }
