f9b6ed363f8e49dea1698a1ca93b9edec9d6b3dd
Smriti Naik <64346825+shruti1421@users.noreply.github.com>
2021-01-16 09:55:33 +0530

    Update 1st_Template.cpp

--- 1st_Template.cpp
+++ 1st_Template.cpp
@@ -49,7 +49,7 @@ bool isPrime(ll n)
      return true;
    if(n%2==0||n%3==0)
      return false;
-   for(ll i=5;i*i<=n;i=i+5)
+   for(ll i=5;i*i<=n;i=i+6)
    {
       if(n%i==0||n%(i+2)==0)
         return false;
